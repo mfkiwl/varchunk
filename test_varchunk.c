e93aa7b945e5e76722086fc4f1324ffcae92fcc9
Hanspeter Portner <dev@open-music-kontrollers.ch>
2022-01-23 16:21:20 +0100

    Make compatible with REUSE spec 3.0
    https://reuse.software/spec/

--- test_varchunk.c
+++ test_varchunk.c
@@ -1,19 +1,7 @@
 /*
- * Copyright (c) 2015-2017 Hanspeter Portner (dev@open-music-kontrollers.ch)
- *
- * This is free software: you can redistribute it and/or modify
- * it under the terms of the Artistic License 2.0 as published by
- * The Perl Foundation.
- *
- * This source is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Artistic License 2.0 for more details.
- *
- * You should have received a copy of the Artistic License 2.0
- * along the source as a COPYING file. If not, obtain it from
- * http://www.perlfoundation.org/artistic_license_2_0.
- */
+* SPDX-FileCopyrightText: (c) Hanspeter Portner (dev@open-music-kontrollers.ch)
+* SPDX-License-Identifier: Artistic-2.0
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
